18bde886cbe9f9f63703251969e85c63e0666eaa
SK5027793 <swatikamat.gp@gmail.com>
2020-05-11 18:12:12 +0530

    Updated Author name in TestCase1

--- TestNGLearning/src/test/java/regression/TestCase1.java
+++ TestNGLearning/src/test/java/regression/TestCase1.java
@@ -8,6 +8,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+//Author: Swati Kamat
 public class TestCase1 
 {   
 	@BeforeTest
